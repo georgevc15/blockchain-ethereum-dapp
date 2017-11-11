4113805f012d57e918a7528e18f2da8a33746dec
George Codat Tudoran <office@solcreation.ro>
2017-11-11 18:28:24 +0200

    get contract balance

--- secondcontract.sol
+++ secondcontract.sol
@@ -2,16 +2,30 @@ pragma solidity ^0.4.0;
 contract MyContract {
     
     uint256 myVariable;
+    
+    address owner;
 
-    function MyContract(){
-        myVariable = 5;    
+    function MyContract() payable {
+        myVariable = 5;
+        owner = msg.sender;
     }
     
     function setMyVariable(uint myNewVariable) {
-        myVariable = myNewVariable;
+        if(msg.sender == owner) {
+            myVariable = myNewVariable;
+        }
     }
     
     function getMyVariable() constant returns(uint) {
         return myVariable;
     }
+    
+    function getMyContractBalance() constant returns(uint256) {
+        return this.balance;
+    }
+    
+    function () payable {
+        
+    }
+    
 }
